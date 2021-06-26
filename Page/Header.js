73ebd724ae57084b7ba84da1a73e0b0a11dcb495
Nur Rahmadani <nagamas.rahma@gmail.com>
2021-06-26 10:56:08 +0800

    TUGAS 3 REACT JS
    Tugas ini kami kumpulkan untuk Niomic

---
+++ Page/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render (){
+    return(
+      <center><b>
+      <a>Home |</a>
+      <a>Product |</a>
+      <a>Kontak |</a>
+      <a>Tentang Kami</a>
+      </b></center>
+    );
+  }
+}
+
+export default Header;
